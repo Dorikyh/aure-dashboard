05b8bddde3e0144745db75659d84d56ecc668b8c
dorikyh <gdorikyh@gmail.com>
2024-03-17 16:11:04 -0500

    Save the jwt token in the browser

--- src/components/Pages/DashboardPage.js
+++ src/components/Pages/DashboardPage.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SideBar from "../SideBar.js";
 
 export const Dashboard = () => {
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get("token");
+    if (token) {
+      // Save the token in the webpage data
+      localStorage.setItem("jwtToken", token);
+    }
+  }, []);
+
   return (
     <div className="dashboard-container">
       <SideBar />
       <main>
-          <div>
-            <h1>Hello world</h1>
-            {/* Agrega aquí más información del usuario según lo que devuelva la API */}
-          </div>
+        <div>
+          <h1>Hello world</h1>
+          {/* Agrega aquí más información del usuario según lo que devuelva la API */}
+        </div>
       </main>
     </div>
   );
